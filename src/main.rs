16e8350ef18b8bd37238b02a0cf1081a361c8ac4
Juyeong Maing <mjy9088@naver.com>
2024-02-26 22:23:54 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let result = line
+        .trim()
+        .split(' ')
+        .flat_map(|x| x.parse::<i32>())
+        .fold(0, |acc, curr| acc + curr);
+
+    println!("{}", result);
+}
